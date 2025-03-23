eb8e9ba374a149f0e4322029498e327cd4d63e85
Tanika <tanikagulati@gmail.com>
2025-03-23 15:29:25 +0530

    practice arrays

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -21,6 +21,42 @@ int findSecondLargest(int arr[], int n)
     return slargest;
 }
 
+int removeDuplicates(int arr[], int n)
+{
+    // Returns number of unique elements
+    int i = 0, j = 1;
+    while (j < n)
+    {
+        if (arr[i] != arr[j])
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+        j++;
+    }
+    return i + 1;
+}
+
+void rotate(int nums[], int size, int k)
+{
+    // Handle if k > size
+    int steps = k % size;
+    // Partition on the basis of k and reverse
+    int pIndex = size - steps;
+    for (int i = 0; i < pIndex / 2; i++)
+    {
+        swap(nums[i], nums[pIndex - 1 - i]);
+    }
+    for (int i = 0; i < steps / 2; i++)
+    {
+        swap(nums[pIndex + i], nums[size - 1 - i]);
+    }
+    for (int i = 0; i < size / 2; i++)
+    {
+        swap(nums[i], nums[size - 1 - i]);
+    }
+}
+
 int main()
 {
     // Max array of size 10^5 can be initialized (10^8 if initialized globally)
@@ -34,4 +70,17 @@ int main()
     }
 
     cout << findSecondLargest(arr, n) << endl;
+
+    // Remove duplicates from sorted array
+    cout << removeDuplicates(arr, n) << endl;
+
+    int k;
+    cin >> k;
+
+    // Rotate the array by k steps (inplace)
+    rotate(arr, n, k);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
