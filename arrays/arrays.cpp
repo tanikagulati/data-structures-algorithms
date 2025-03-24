eaa1c011e8eaa4407fd0c7d767220d0f4f6b5341
Tanika <tanikagulati@gmail.com>
2025-03-24 23:43:06 +0530

    practice arrays

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -134,6 +134,29 @@ vector<int> intersectionBruteF(vector<int> &v1, vector<int> &v2)
 
 vector<int> intersectionOptimized(vector<int> &v1, vector<int> &v2)
 {
+    int n1 = v1.size();
+    int n2 = v2.size();
+    vector<int> res;
+
+    int i = 0, j = 0;
+    while (i < n1 && j < n2)
+    {
+        if (v1[i] < v2[j])
+        {
+            i++; // No match
+        }
+        else if (v2[j] < v1[i])
+        {
+            j++;
+        }
+        else
+        {
+            res.push_back(v1[i]);
+            i++;
+            j++;
+        }
+    }
+    return res;
 }
 
 int main()
