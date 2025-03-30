bed295818623a0f05d2d3d991924e7a107c8167f
Tanika <tanikagulati@gmail.com>
2025-03-30 19:43:07 +0530

    practice arrays

--- arrays/sort_array_012.cpp
+++ arrays/sort_array_012.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sort012(vector<int> &v)
+void sort012Better(vector<int> &v)
 {
     int n = v.size();
     int hash[3] = {0};
@@ -25,6 +25,34 @@ void sort012(vector<int> &v)
         v[i] = 2;
         i++;
     }
+    // TC: O(2n)
+    // SC: O(1)
+}
+
+void sort012Optimal(vector<int> &v)
+{
+    int n = v.size();
+    int l = 0, m = 0, h = n - 1;
+    while (m <= h)
+    {
+        if (v[m] == 0)
+        {
+            swap(v[l], v[m]);
+            l++;
+            m++;
+        }
+        else if (v[m] == 1)
+        {
+            m++;
+        }
+        else if (v[m] == 2)
+        {
+            swap(v[h], v[m]);
+            h--;
+        }
+    }
+    // TC: O(n)
+    // SC: O(1)
 }
 
 int main()
@@ -36,7 +64,15 @@ int main()
     {
         cin >> v[i];
     }
-    sort012(v);
+
+    // Brute -> Sorting algo
+
+    // Better
+    sort012Better(v);
+
+    // Optimal: Dutch national flag algorithm
+    sort012Optimal(v);
+
     for (int i = 0; i < n; i++)
     {
         cout << v[i] << " ";
