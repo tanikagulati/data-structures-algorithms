20db2ca14f14edb7ed2b11530f49faa74ae8cfd9
Tanika <tanikagulati@gmail.com>
2025-04-01 01:49:22 +0530

    practice arrays

--- arrays/max_subarray_sum.cpp
+++ arrays/max_subarray_sum.cpp
@@ -24,6 +24,39 @@ int maxSubarraySum(vector<int> &v)
     // SC: O(1)
 }
 
+int maxSubarraySum2(vector<int> &v)
+{
+    int n = v.size();
+    long long sum = 0;
+    int max = LONG_MIN;
+    int a = 0, b = 0; // Indexes of result subarray
+    int temp = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (sum == 0)
+        {
+            temp = i; // Potential result start index
+        }
+        sum += v[i];
+        if (sum > max)
+        {
+            max = sum;
+            // Update indexes only while updating max
+            b = i;
+            a = temp;
+        }
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    cout << a << " " << b << endl;
+    return max;
+    // TC: O(n)
+    // SC: O(1)
+}
+
 int main()
 {
     int n;
@@ -35,5 +68,10 @@ int main()
     }
 
     // Brute: Check each subarray
-    cout << maxSubarraySum(v) << endl;
+    // cout << maxSubarraySum(v) << endl;
+
+    // Optimal: Kadane's algo
+    cout << maxSubarraySum2(v) << endl;
+
+    // Follow up: Try with divide and conquer
 }
