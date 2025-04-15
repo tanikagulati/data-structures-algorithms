51c3b9c16491f563c14770494a7ca6aae2775095
Tanika <tanikagulati@gmail.com>
2025-04-16 03:08:48 +0530

    practice arrays

--- arrays/reverse_pairs.cpp
+++ arrays/reverse_pairs.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int mergeSortedArrays(vector<int> &v, int low, int mid, int high)
+void mergeSortedArrays(vector<int> &v, int low, int mid, int high)
 {
     vector<int> res;
     int left = low;
     int right = mid + 1;
-    int count = 0;
 
     while (left <= mid && right <= high)
     {
@@ -19,7 +18,6 @@ int mergeSortedArrays(vector<int> &v, int low, int mid, int high)
         {
             res.push_back(v[right]);
             right++;
-            count += mid - left + 1;
         }
     }
 
@@ -39,35 +37,52 @@ int mergeSortedArrays(vector<int> &v, int low, int mid, int high)
     {
         v[i] = res[i - low];
     }
+}
 
+int countPairs(vector<int> &v, int low, int mid, int high)
+{
+    int count = 0;
+    int right = mid + 1;
+
+    for (int i = low; i <= mid; i++)
+    {
+        while (right <= high && v[i] > 2 * v[right])
+        {
+            right++;
+        }
+        count += right - (mid + 1);
+    }
     return count;
 }
 
-int countInversions(vector<int> &v, int low, int high)
+int reversePairs(vector<int> &v, int low, int high)
 {
     int count = 0;
     if (low == high)
         return count;
 
     int mid = (low + high) / 2;
-    count += countInversions(v, low, mid);
-    count += countInversions(v, mid + 1, high);
-    count += mergeSortedArrays(v, low, mid, high);
+    count += reversePairs(v, low, mid);
+    count += reversePairs(v, mid + 1, high);
+    count += countPairs(v, low, mid, high);
+    mergeSortedArrays(v, low, mid, high);
     return count;
 
-    // TC: O(nlogn)
+    // TC: O(2nlogn)
     // SC: O(n)
 }
 
 int main()
 {
-    // Brute: Iterate for each element and check
+    // i<j & a[i] > 2*a[j]
+
+    // Brute: Generate all pairs and check -> O(n^2)
 
     // Optimal: Using merge sort
 
-    vector<int> v = {5, 3, 2, 1, 4};
+    vector<int> v = {1, 3, 2, 3, 1};
     int n = v.size();
-    cout << countInversions(v, 0, n - 1) << endl;
+    cout << reversePairs(v, 0, n - 1) << endl;
 
     for (auto i : v)
     {
