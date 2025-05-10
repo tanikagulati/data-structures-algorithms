91fe58419ea6097b565f2946bc8102e1bca79b92
Tanika <tanikagulati@gmail.com>
2025-05-10 23:59:25 +0530

    binary search

--- binary search/minimize_max_dist_bw_gas_stations.cpp
+++ binary search/minimize_max_dist_bw_gas_stations.cpp
@@ -42,27 +42,82 @@ double minimizeMaxDistanceBrute(vector<int> &stations, int k)
     // SC: O(1)
 }
 
-double minimizeMaxDistanceOptimal(vector<int> &stations, int k)
+double minimizeMaxDistanceBetter(vector<int> &stations, int k)
 {
     int n = stations.size();
     vector<int> sections(n - 1);
 
-    priority_queue<int> pq;
+    priority_queue<pair<double, int>> pq;
 
-    // Store the differences in pq
-    for (int i = 0; i < n; i++)
+    // Store the initial differences with its index in pq
+    for (int i = 0; i < n - 1; i++)
     {
-        double diff = double(stations[i + 1] - stations[i]);
-        pq.push(diff);
+        double diff = stations[i + 1] - stations[i];
+        pq.push({diff, i});
     }
+
+    int maxInd;
+    double newSectionLength;
+
+    for (int i = 0; i < k; i++)
+    {
+        maxInd = pq.top().second;
+        pq.pop();
+        sections[maxInd]++; // 2 0 1
+        newSectionLength = double(stations[maxInd + 1] - stations[maxInd]) / double(sections[maxInd] + 1);
+        pq.push({newSectionLength, maxInd});
+    }
+
+    return pq.top().first;
+    // TC: O(nlogn + klogn)
+    // SC: O(n-1)
+}
+
+int findRequiredStations(vector<int> &stations, long double mid)
+{
+    // TODO: Try some time later
+}
+
+double minimizeMaxDistanceOptimal(vector<int> &stations, int k)
+{
+    int n = stations.size();
+    long double low = 0;
+    long double high = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        long double diff = stations[i + 1] - stations[i];
+        high = max(high, diff);
+    }
+    long double mid;
+
+    while (high - low < 10e-6)
+    {
+        mid = low + ((high - low) / 2);
+        int reqdStations = findRequiredStations(stations, mid);
+        if (reqdStations > k)
+        {
+            low = mid;
+        }
+        else
+        {
+            high = mid;
+        }
+    }
+    return high;
 }
 
 int main()
 {
     vector<int> stations = {1, 13, 17, 23};
-    int k = 5;
+    int k = 12;
 
+    // Brute
     cout << minimizeMaxDistanceBrute(stations, k) << endl;
 
+    // Better using PQ
+    cout << minimizeMaxDistanceBetter(stations, k) << endl;
+
+    // Optimal using BS
     // cout << minimizeMaxDistanceOptimal(stations, k) << endl;
 }
