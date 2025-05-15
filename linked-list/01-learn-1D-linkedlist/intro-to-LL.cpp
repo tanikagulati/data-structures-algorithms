59a09e0b631d3c06d3ae93fd02a893035a9ba1cc
Tanika <tanikagulati@gmail.com>
2025-05-16 00:55:08 +0530

    learn 1D linkedlist

---
+++ linked-list/01-learn-1D-linkedlist/intro-to-LL.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+};
+
+Node *convertArrayToLL(int arr[], int n)
+{
+    Node *head = new Node(arr[0], nullptr);
+    Node *mover = head; // Never tamper the head
+    for (int i = 1; i < n; i++)
+    {
+        Node *temp = new Node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+    // TC: O(n)
+}
+
+int lengthOfLL(Node *head)
+{
+    Node *temp = head;
+    int cnt = 0;
+    while (temp)
+    {
+        cnt++;
+        temp = temp->next;
+    }
+    return cnt;
+    // TC: O(n)
+}
+
+int checkIfPresent(Node *head, int target)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        if (temp->data == target)
+            return 1;
+        temp = temp->next;
+    }
+    return 0;
+    // TC: O(n)
+}
+
+int main()
+{
+    // Convert array to LL
+    int arr[4] = {1, 2, 3, 4};
+    Node *head = convertArrayToLL(arr, 4);
+
+    // Traversal of LL -> O(n)
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+
+    // Length of LL
+    cout << lengthOfLL(head) << endl;
+
+    // Search in LL
+    cout << checkIfPresent(head, 3) << endl;
+}
