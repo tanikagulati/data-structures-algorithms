65e8f11c9136916bdca78d0837ffe8d4f61c1644
tanikagulati <tanikagulati@gmail.com>
2025-06-24 02:40:02 +0530

    recursion medium problems

---
+++ recursion/02-subsequences-pattern/combination_sum.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void combinationSum(vector<int> &candidates, int target, int index, vector<int> &temp, vector<vector<int>> &res)
+{
+    // target == 0 can be a base case, but this is the generalized version (avoiding too many conditions)
+    if (index == candidates.size())
+    {
+        if (target == 0)
+        {
+            res.push_back(temp);
+        }
+        return;
+    }
+    // Pick
+    if (candidates[index] <= target)
+    {
+        temp.push_back(candidates[index]);
+        combinationSum(candidates, target - candidates[index], index, temp, res);
+        temp.pop_back();
+    }
+    // Don't pick
+    combinationSum(candidates, target, index + 1, temp, res);
+
+    // TC: O(2^t + (K*k))
+    // where 2^t = estimate no of combinations (worst case),
+    // K = no of valid combinations, k = avg length of valid combination
+    // SC: O(t*K) -> For result + O(t) -> Recursion stack space
+}
+
+int main()
+{
+    vector<int> candidates = {2, 3, 6, 7};
+    int target = 7;
+    vector<vector<int>> res;
+    vector<int> temp = {};
+
+    combinationSum(candidates, target, 0, temp, res);
+
+    for (auto i : res)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
