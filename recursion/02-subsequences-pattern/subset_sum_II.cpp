12be900f692cb5c4a643ffd5042085f8454ededd
tanikagulati <tanikagulati@gmail.com>
2025-06-26 23:59:25 +0530

    recursion medium problems

---
+++ recursion/02-subsequences-pattern/subset_sum_II.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+void subsetSum2(vector<int> &v, int index, vector<int> &temp, vector<vector<int>> &res)
+{
+    res.push_back(temp);
+    if (index == v.size())
+    {
+        return;
+    }
+
+    for (int i = index; i < v.size(); i++)
+    {
+        if (i > index && temp[i] == temp[i - 1])
+        {
+            continue;
+        }
+        temp.push_back(v[i]);
+        subsetSum2(v, i + 1, temp, res);
+        temp.pop_back();
+    }
+    // TC: O(2^n * k) -> k = avg length
+    // SC: O(2^n * k) For result + O(n) Recursion stack space
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 2};
+    vector<int> temp;
+    vector<vector<int>> res;
+
+    subsetSum2(v, 0, temp, res);
+
+    for (auto i : res)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
