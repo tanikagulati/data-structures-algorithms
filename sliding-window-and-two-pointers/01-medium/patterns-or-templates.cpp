7d085bc042db3d7ef9fb076939e58fe2a34a260f
tanikagulati <tanikagulati@gmail.com>
2025-07-23 23:59:39 +0530

    learning sliding window and two pointers

--- sliding-window-and-two-pointers/01-medium/patterns-or-templates.cpp
+++ sliding-window-and-two-pointers/01-medium/patterns-or-templates.cpp
@@ -48,19 +48,85 @@ int longestSubarrayBrute(vector<int> &nums, int k)
     // SC: O(1)
 }
 
+int longestSubarrayBetter(vector<int> &nums, int k)
+{
+    // Two pointers (expand and shrink)
+    int n = nums.size();
+    int l = 0, r = 0;
+    int sum = 0;
+    int maxLen = 0;
+
+    while (r < n)
+    {
+        sum += nums[r];
+        while (sum > k && l <= r)
+        {
+            sum -= nums[l];
+            l++;
+        }
+        if (sum <= k)
+        {
+            maxLen = max(maxLen, r - l + 1);
+        }
+        r++;
+    }
+    return maxLen;
+    // TC: O(2n)
+    // SC: O(1)
+}
+
+int longestSubarrayOptimal(vector<int> &nums, int k)
+{
+    // Two pointers (expand and shrink)
+    int n = nums.size();
+    int l = 0, r = 0;
+    int sum = 0;
+    int maxLen = 0;
+
+    while (r < n)
+    {
+        sum += nums[r];
+        if (sum > k && l <= r)
+        {
+            sum -= nums[l];
+            l++;
+        }
+        if (sum <= k)
+        {
+            maxLen = max(maxLen, r - l + 1);
+        }
+        r++;
+    }
+    return maxLen;
+    // TC: O(n)
+    // SC: O(1)
+}
+
 int main()
 {
     // Pattern 1: Constant window (v.easy)
     // Eg. Maximum subarray sum having window size k
-    vector<int> nums = {-1, 2, 3, 3, 4, 5, -1};
-    int k = 4;
-    cout << subarraySum(nums, 4) << endl;
+    vector<int> nums1 = {-1, 2, 3, 3, 4, 5, -1};
+    int k = 10;
+    cout << subarraySum(nums1, 4) << endl;
 
     // Pattern 2: Longest subarray/substring where <condition> (imp)
     // Eg. Longest subarray with sum <= k
 
-    // Brute:
-    cout << longestSubarrayBrute(nums, k) << endl;
+    // Note: Assuming array elements are positive, sliding window will not work with negatives
 
-    // Better:
+    vector<int> nums2 = {2, 3, 1, 1, 2, 1, 10};
+
+    cout << longestSubarrayBrute(nums2, k) << endl;
+    cout << longestSubarrayBetter(nums2, k) << endl;
+    cout << longestSubarrayOptimal(nums2, k) << endl;
+
+    // Pattern 3: No. of subarrays where <condition>
+    // Eg. No. of subarrays where sum = k
+
+    // Soln: (No. of subarrays where sum <= k) - (No. of subarrays where sum <= k-1)
+    // Because it is difficult to determine when to expand/shrink for sum = k case
+
+    // Pattern 4: Shortest/Minimum window where <condition>
+    // Approach: Find a valid window and try to shrink it down until valid
 }
