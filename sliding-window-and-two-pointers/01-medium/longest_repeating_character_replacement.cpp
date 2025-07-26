b20a1d5516ef1e4dc3c1f305b667a6e9b4d832eb
tanikagulati <tanikagulati@gmail.com>
2025-07-26 23:35:49 +0530

    sliding window and two pointers problems

--- sliding-window-and-two-pointers/01-medium/longest_repeating_character_replacement.cpp
+++ sliding-window-and-two-pointers/01-medium/longest_repeating_character_replacement.cpp
@@ -14,9 +14,9 @@ int characterReplacement(string &s, int k)
         int maxFreq = 0;
         for (int j = i; j < n; j++)
         {
-            hash[s[i] - 'A']++;
-            maxFreq = max(maxFreq, hash[s[i]]);
-            changes = (j - i + 1) - maxLen;
+            hash[s[j] - 'A']++;
+            maxFreq = max(maxFreq, hash[s[j] - 'A']);
+            changes = (j - i + 1) - maxFreq;
             if (changes <= k)
                 maxLen = max(maxLen, j - i + 1);
             else
@@ -28,10 +28,39 @@ int characterReplacement(string &s, int k)
     // SC: O(1)
 }
 
+int characterReplacementOptimal(string &s, int k)
+{
+    int n = s.size();
+    int maxLen = 0;
+    int l = 0, r = 0;
+    int maxFreq = 0, changes = 0;
+    vector<int> hash(26, 0);
+    while (r < n)
+    {
+        hash[s[r] - 'A']++;
+        maxFreq = max(maxFreq, hash[s[r] - 'A']);
+        changes = (r - l + 1) - maxFreq;
+        if (changes <= k)
+            maxLen = max(maxLen, r - l + 1);
+        else
+        {
+            hash[s[l] - 'A']--;
+            // Updating maxFreq at this step is not reqd, because a lower maxFreq will never contribute to answer
+            l++;
+        }
+        r++;
+    }
+    return maxLen;
+    // TC: O(n)
+    // SC: O(1)
+}
+
 int main()
 {
     string s = "AABABBA";
     int k = 1;
 
     cout << characterReplacement(s, k) << endl;
+
+    cout << characterReplacementOptimal(s, k) << endl;
 }
